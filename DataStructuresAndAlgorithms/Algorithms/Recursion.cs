a8adc6f9deba0d3e7ace791047f7a57ec38074e4
Rob Sangar <robsangar@gmail.com>
2020-07-16 16:28:26 +0100

    Notes on basic recursion.

---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.Algorithms
+{
+    public class Recursion
+    {
+        
+        private int Counter { get; set; }
+
+        public Recursion()
+        {
+            this.Counter = 0;
+        }
+
+        //Basic recursion
+        //Will keep calling itself until counter > 3.
+        //Each subsequent call will be added to the call stack. Without a base case, recusion can result in a stack overflow.
+        //Note the "return" keyword before the call to itself - this ensures the "Done!" value bubbles back up to, and is returned by, the first call to Inception. 
+        public string Inception()
+        {
+            if (this.Counter > 3)    //Base case
+            {
+                return "Done!";
+            }
+
+            this.Counter++;
+            return Inception();      //Recursion case
+        }
+
+
+
+    }
+}
