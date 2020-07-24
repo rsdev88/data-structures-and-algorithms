3fe80dd7181eab03ec8ebc21eaab9e2b3a20a50f
Rob Sangar <robsangar@gmail.com>
2020-07-24 18:33:46 +0100

    Added memoized implementation of recursive Fibonacci

---
+++ DataStructuresAndAlogrithmsTests/Algorithms/DynamicProgrammingTests.cs
@@ -0,0 +1,34 @@
+﻿using DataStructuresAndAlgorithms.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresAndAlogrithmsTests.Algorithms
+{
+    [TestClass]
+    public class DynamicProgrammingTests
+    {
+        [TestMethod]
+        public void MemoizedFibonacci_Tests()
+        {
+            //Arrange 
+            var tester = new DynamicProgramming();
+
+            //Act - see assertions
+
+            //Assert
+            Assert.AreEqual(0, tester.Memoized_Fibonacci(0));
+            Assert.AreEqual(1, tester.Memoized_Fibonacci(1));
+            Assert.AreEqual(1, tester.Memoized_Fibonacci(2));
+            Assert.AreEqual(2, tester.Memoized_Fibonacci(3));
+            Assert.AreEqual(3, tester.Memoized_Fibonacci(4));
+            Assert.AreEqual(5, tester.Memoized_Fibonacci(5));
+            Assert.AreEqual(8, tester.Memoized_Fibonacci(6));
+            Assert.AreEqual(13, tester.Memoized_Fibonacci(7));
+            Assert.AreEqual(21, tester.Memoized_Fibonacci(8));
+            Assert.AreEqual(34, tester.Memoized_Fibonacci(9));
+            Assert.AreEqual(55, tester.Memoized_Fibonacci(10));
+            Assert.AreEqual(89, tester.Memoized_Fibonacci(11));
+            Assert.AreEqual(144, tester.Memoized_Fibonacci(12));
+        }
+
+    }
+}
